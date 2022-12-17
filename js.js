17acfcf381c8332ec268ab965f4ae0a317d85ef4
FernandaAl2 <fernandaalves2021mogi@gmail.com>
2022-12-17 17:32:33 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,16 @@
+function validar(){
+   var elemento="documento.body"
+ elemento.classlist.body.toggle ("validar")
+}
+function Enviar(){
+    let nome= document.querySelectorAll("Nome");
+    let email= document.querySelectorAll("email");
+}
+if (elemento.value != ""){
+    alert ("ja esta preenchido")
+
+}
+else{
+    alert("Tem nada aqui")
+}
+
